a9db7c67b30aa7a63b1c9b451a576c465a236828
z00t <clutchgaming@gmail.com>
2019-12-30 11:01:26 -0500

    Add required fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,7 @@
+client_script 'lib/redis.lua'
+server_script 'wtf_redis.js'
+
+games { 'rdr3', 'gta5' }
+
+fx_version 'adamant'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
